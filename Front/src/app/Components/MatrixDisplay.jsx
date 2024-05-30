3c78404f5c964773bd3c23b5419f516f8263025d
JIsraP <a01423294@tec.mx>
2024-05-29 21:07:01 -0600

    Matrix navigator to highlighter

--- Front/src/app/Components/MatrixDisplay.jsx
+++ Front/src/app/Components/MatrixDisplay.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Grid, Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export const MatrixDisplay = ({ matrix }) => {
+    const navigate = useNavigate();
     const { size, data } = matrix;
     const adjustedSize = size + 1;
 
@@ -20,19 +22,30 @@ export const MatrixDisplay = ({ matrix }) => {
                 return 'App.red';
             }
         } else if (typeof value === 'string') {
-            if (value == " ") {
-                return 'App.lightgrey'
+            if (value === " ") {
+                return 'App.lightgrey';
             }
         }
         return 'transparent';
+    };
+
+    const handleClick = (val, rowIndex, colIndex) => {
+        if (typeof val === 'number') {
+            // Find the file names in the current row and column
+            const rowFiles = rows[rowIndex].filter(item => typeof item === 'string' && item.includes('.py'));
+            const colFiles = rows.map(row => row[colIndex]).filter(item => typeof item === 'string' && item.includes('.py'));
+            console.log(`Row files: ${rowFiles}, Column files: ${colFiles}`);
+            const file_names = [rowFiles, colFiles]
+            navigate('/highlighter', { state: { file_names } });
+        }
     };
 
     return (
         <Grid container justifyContent="center" alignItems="center">
             {rows.map((row, rowIndex) => (
                 <Grid container item key={rowIndex} padding={1} spacing={1} justifyContent="center">
-                    {row.map((val, index) => (
-                        <Grid item key={index}>
+                    {row.map((val, colIndex) => (
+                        <Grid item key={colIndex}>
                             <Button
                                 variant="contained"
                                 sx={{
@@ -46,6 +59,7 @@ export const MatrixDisplay = ({ matrix }) => {
                                         boxShadow: 'none'
                                     }
                                 }}
+                                onClick={() => handleClick(val, rowIndex, colIndex)}
                             >
                                 <Typography variant="body1" sx={{ color: typeof val === 'number' ? 'App.white' : 'primary.main' }}>{val}</Typography>
                             </Button>
