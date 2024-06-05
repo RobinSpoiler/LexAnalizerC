0067511b7d471234b20ea2e58028a937d16f9144
RobinSpoiler <ksoalianza@gmail.com>
2024-06-04 19:48:46 -0600

    new tests

--- TestCases/Test1c.py
+++ TestCases/Test1c.py
@@ -1,12 +1,18 @@
-def es_palabra_palindromo(palabra):
-    palabra = palabra.replace(" ", "").lower()
-    reverso = palabra[::-1]
-    return palabra == reverso
-def main():
-    palabra = input("Introduce una palabra o frase: ")
-    if es_palabra_palindromo(palabra):
-        print(f"'{palabra}' es un palíndromo")
+def clasificar_triangulo(l1, l2, l3):
+    lados = [l1, l2, l3]
+    conteo = {lado: lados.count(lado) for lado in lados}
+    if 3 in conteo.values():
+        return "Equilátero"
+    elif 2 in conteo.values():
+        return "Isósceles"
     else:
-        print(f"'{palabra}' no es un palíndromo")
+        return "Escaleno"
 if __name__ == "__main__":
-    main()
+    conjuntos_de_lados = [
+        (2, 2, 2),
+        (7, 7, 10),
+        (3, 4, 6)
+    ]
+    for l1, l2, l3 in conjuntos_de_lados:
+        tipo = clasificar_triangulo(l1, l2, l3)
+        print(f"Triángulo con lados {l1}, {l2}, {l3} es {tipo}")
